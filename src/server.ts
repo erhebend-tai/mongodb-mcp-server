7934b565b0a5752bebc52e3199431d5a1309164b
erhebend-tai <erhebend@protonmail.com>
2025-06-27 06:22:41 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -45,7 +45,7 @@ export class Server {
         // the tool accepts any arguments, even if they're all optional.
         //
         // see: https://github.com/modelcontextprotocol/typescript-sdk/blob/131776764536b5fdca642df51230a3746fb4ade0/src/server/mcp.ts#L705
-        // Since paramsSchema here is not undefined, the server will create a non-optional z.object from it.
+        // Since paramsSchema here is not undefined, the server will create a non-optional z.object from it. *Shrug
         const existingHandler = (
             this.mcpServer.server["_requestHandlers"] as Map<
                 string,
